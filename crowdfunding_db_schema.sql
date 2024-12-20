a074761b5bf57d6ebd1861689ada24916731c4df
valjk <val.keeranan@gmail.com>
2024-12-19 21:40:17 -0800

    Commit schema changes

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -2,41 +2,38 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/LNPjKf
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
 
 CREATE TABLE "Campaign" (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
-    "company_name" object   NOT NULL,
-    "description" object   NOT NULL,
+    "company_name" varchar   NOT NULL,
+    "description" varchar   NOT NULL,
     "goal" float   NOT NULL,
     "pledged" float   NOT NULL,
-    "outcome" object   NOT NULL,
+    "outcome" varchar   NOT NULL,
     "backers_count" int   NOT NULL,
-    "country" object   NOT NULL,
-    "currency" object   NOT NULL,
+    "country" varchar   NOT NULL,
+    "currency" varchar   NOT NULL,
     "launched_date" int   NOT NULL,
     "end_date" int   NOT NULL,
-    "category_id" object   NOT NULL,
-    "subcategory_id" object   NOT NULL,
+    "category_id" varchar   NOT NULL,
+    "subcategory_id" varchar   NOT NULL,
     CONSTRAINT "pk_Campaign" PRIMARY KEY (
         "cf_id"
      )
 );
 
 CREATE TABLE "Category" (
-    "category_id" object   NOT NULL,
-    "category" object   NOT NULL,
+    "category_id" varchar   NOT NULL,
+    "category" varchar   NOT NULL,
     CONSTRAINT "pk_Category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "Subcategory" (
-    "subcategory_id" object   NOT NULL,
-    "subcategory" object   NOT NULL,
+    "subcategory_id" varchar   NOT NULL,
+    "subcategory" varchar   NOT NULL,
     CONSTRAINT "pk_Subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -44,9 +41,9 @@ CREATE TABLE "Subcategory" (
 
 CREATE TABLE "Contacts" (
     "contact_id" int   NOT NULL,
-    "first_name" object   NOT NULL,
-    "last_name" object   NOT NULL,
-    "email" object   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "email" varchar   NOT NULL,
     CONSTRAINT "pk_Contacts" PRIMARY KEY (
         "contact_id"
      )
